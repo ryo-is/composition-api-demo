ae1492cb9a67d3ae23b61de97dfcd50f87e22b94
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-10-02 14:28:12 +0900

    feat:CompositionAPI入門

--- src/views/home/script.ts
+++ src/views/home/script.ts
@@ -1,15 +1,25 @@
-import Vue from "vue"
+import { createComponent, reactive } from "@vue/composition-api"
 import { HomeComponentState } from "@/types"
 
-export default Vue.extend({
-  data(): HomeComponentState {
-    return {
-      text: "hoge"
+export default createComponent({
+  setup() {
+    const state = reactive<HomeComponentState>({
+      text: "Hello Vue Composition API",
+      count: 0
+    })
+
+    const addCount = (): void => {
+      state.count += 1
     }
-  },
-  methods: {
-    getText(): string {
-      return this.text
+
+    const minusCount = (): void => {
+      state.count -= 1
+    }
+
+    return {
+      state,
+      addCount,
+      minusCount
     }
   }
 })
